f4c58b7691a6bb81eea015f48c4bb2795557cf90
Stanisof <128597372+Stanisof@users.noreply.github.com>
2024-03-29 13:52:31 +0200

    Delete notes button removes from DOM and array

--- src/index.js
+++ src/index.js
@@ -17,28 +17,32 @@ class Note {
     }
 }
 
-let homeArray = []
+let projects = []
+
 
 function homeComponent() {
-                                                        console.log(homeArray)
+    let homeArray = [];
+    projects.push(homeArray)
+console.log(homeArray)
 
-    displayElement(homeArray);
-
+    displayElement(projects[0]);
+console.log(projects)
     openNoteForm.addEventListener('click', () => noteDialog.showModal());
 
     noteForm.addEventListener('submit', (e) => {
         e.preventDefault();
+        e.stopImmediatePropagation();
+
         const fd = new FormData(noteForm);
 
-        let project = new Note(fd.get('task'), fd.get('description'), fd.get('duedate'), fd.get('priority'));
-        pushToArray(homeArray, project);
-
-        //content.appendChild(createNote())
-        displayElement(homeArray);
+        let note = new Note(fd.get('task'), fd.get('description'), fd.get('duedate'), fd.get('priority'));
+        
+        pushToArray(projects[0], note);
+        displayElement(projects[0]);
 
         noteDialog.close();
         noteForm.reset();
-        console.log(homeArray)
+        console.log(projects[0])
     })
 }
 
@@ -64,13 +68,15 @@ function displayElement(array) {
         
         if(!isCreated) {
             
-            saveIndex(array[i], array)
+            array[i].index = i;
 
             content.appendChild(createNote());
             cacheNoteElements().title.textContent = array[i].title;
             cacheNoteElements().description.textContent = array[i].description;
             cacheNoteElements().duedate.textContent = array[i].duedate;
             cacheNoteElements().noteContainer.setAttribute('data-priority', array[i].priority) 
+            
+            removeFromArray(array, array[i], cacheNoteElements().deleteButton)
         }
     }
 
@@ -80,42 +86,33 @@ function pushToArray(array, object) {
     array.push(object)
 }
 
-/* function removeFromArray(array, object) {
+function removeFromArray(array, object, button) {
 
-    deleteButton.addEventListener('click', () => {
-      array.splice(object.dataIndex,1);
+    button.addEventListener('click', () => {
+      array.splice(object.index,1);
   
       array.forEach((item)=> {
         if(item.dataIndex > object.dataIndex) 
         item.dataIndex -= 1;
       })
-      console.log(myLibrary);
-      div.remove();
+      console.log(array);
+      //noteContainer.remove();
   
     })
-  } */
+  } 
 
-  function saveIndex(object, array) {
-    if(array.length === 0) {
-        object.index = 0
-    } else {
-        object.index = array.length - 1;
-    }
-  }
 
 home.addEventListener('click', ()=> homeComponent());
-
-
-
-
 
 
 let noteIns = new Note("Task", "Long description of words", "2024-03-21", "middle");
 let newTask = new Note("Cleaning", "I have to clean the whole godamn house", "2024-04-10", "low")
 let task = new Note("coding", "i have to code a lot and it huerts", "2024-03-30", "high");
-saveIndex(noteIns, homeArray)
-pushToArray(homeArray, noteIns);
-pushToArray(homeArray, newTask);
-pushToArray(homeArray, task);
 
 homeComponent();
+
+pushToArray(projects[0], noteIns);
+pushToArray(projects[0], newTask);
+pushToArray(projects[0], task);
+
+console.log(projects)
